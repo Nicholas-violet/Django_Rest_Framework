0dd5606df580ef9c0efb4df1d1a729c30ef7a243
Nocholas_violet <Nocholas_violet@163.com>
2020-07-08 21:37:40 +0800

    02 serializers序列化器

--- DRFdemo/BookTest/models.py
+++ DRFdemo/BookTest/models.py
@@ -1,53 +1,54 @@
 from django.db import models
 
-# Create your models here.
-# from __future__ import unicode_literals
-
-from django.db import models
-from rest_framework import serializers
-# Create your models here.
+#定义图书模型类BookInfo
 class BookInfo(models.Model):
-    """图书模型类"""
-    btitle = models.CharField(max_length=20, verbose_name='标题')
+    btitle = models.CharField(max_length=20, verbose_name='名称')
     bpub_date = models.DateField(verbose_name='发布日期')
     bread = models.IntegerField(default=0, verbose_name='阅读量')
     bcomment = models.IntegerField(default=0, verbose_name='评论量')
-    is_delete = models.BooleanField(default=False, verbose_name='删除标记')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+    # image = models.ImageField(upload_to='booktest', verbose_name='图片', null=True)
+
+    # heros隐藏字段，代表  当前主表对象关联的"多个"从表"HeroInfo对象"
+
 
     class Meta:
-        db_table = 'tb_books'
+        db_table = 'tb_books'  # 指明数据库表名
+        verbose_name = '图书'  # 在admin站点中显示的名称
+        verbose_name_plural = verbose_name  # 显示的复数名称
 
     def __str__(self):
+        """定义每个数据对象的显示信息"""
         return self.btitle
 
-
-
-'''
-class BookInfoSerializer(serializers.Serializer):
-    """图书数据序列化器"""
-    id = serializers.IntegerField(label='ID', read_only=True)
-    btitle = serializers.CharField(label='名称', max_length=20)
-    bpub_date = serializers.DateField(label='发布日期', required=False)
-    bread = serializers.IntegerField(label='阅读量', required=False)
-    bcomment = serializers.IntegerField(label='评论量', required=False)
-    image = serializers.ImageField(label='图片', required=False)
-'''
-
-
+#定义英雄模型类HeroInfo
 class HeroInfo(models.Model):
-    """英雄模型类"""
     GENDER_CHOICES = (
-        (0, '男'),
-        (1, '女')
+        (0, 'female'),
+        (1, 'male')
     )
     hname = models.CharField(max_length=20, verbose_name='名称')
     hgender = models.SmallIntegerField(choices=GENDER_CHOICES, default=0, verbose_name='性别')
-    hcomment = models.CharField(max_length=200, null=True, verbose_name='备注')
-    is_delete = models.BooleanField(default=False, verbose_name='删除标记')
-    hbook = models.ForeignKey('BookInfo', related_name='heros', on_delete=models.CASCADE, verbose_name='所属图书')
+    hcomment = models.CharField(max_length=200, null=True, verbose_name='描述信息')
+    hbook = models.ForeignKey(BookInfo, related_name='heros', on_delete=models.CASCADE, verbose_name='图书')  # 外键
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
 
     class Meta:
         db_table = 'tb_heros'
+        verbose_name = '英雄'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.hname
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': '192.168.203.151',  # 数据库主机
+#         'PORT': 3306,  # 数据库端口
+#         'USER': 'weiwei',  # 数据库用户名
+#         'PASSWORD': '12345',  # 数据库用户密码
+#         'NAME': 'django_demo'  # 数据库名字
+#     }
+# }
--- DRFdemo/BookTest/serializers.py
+++ DRFdemo/BookTest/serializers.py
@@ -1,12 +1,124 @@
 
 
+# 针对BookInfo模型类数据，定义一个BookInfoSerializer序列化器
+# 来对BooKinfo数据进行序列化操作
+
 from rest_framework import serializers
 
-class BookInfoSerializers(serializers.Serializer):
+# # 我们自己心里清楚，这个序列化器是针对BookInfo的！！！
+# class BookInfoSerializer(serializers.Serializer):
+#     # 通过指定同名类属性的形式，来定义转化结果字典中的属性
+#
+#     btitle = serializers.CharField()
+#     bpub_date = serializers.DateField()
+#     bread = serializers.IntegerField()
+#     bcomment = serializers.IntegerField()
+#     is_delete = serializers.BooleanField()
+#     image = serializers.ImageField()
+
+
+class HeroInfoSerializer2(serializers.Serializer):
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female')
+    )
     id = serializers.IntegerField(label='ID', read_only=True)
-    btitle = serializers.CharField(max_length=20, verbose_name='标题')
-    bpub_date = serializers.DateField(verbose_name='发布日期')
-    bread = serializers.IntegerField(default=0, verbose_name='阅读量')
-    bcomment = serializers.IntegerField(default=0, verbose_name='评论量')
-    is_delete = serializers.BooleanField(default=False, verbose_name='删除标记')
+    hname = serializers.CharField(label='名字', max_length=20)
+    hgender = serializers.ChoiceField(choices=GENDER_CHOICES, label='性别', required=False)
+
+
+# 定义一个针对btitle的校验函数
+def check_btitle(value):
+    # 参数value：经过前序校验之后的btitle数据
+    # 我们通过抛出ValidationError异常表示校验失败！！
+    # 返回值无
+
+    # 如果"django"字符串不再value中，表示不符合格式
+    # {"btitle": "围城"}
+    if "django" not in value:
+        # 不是一本关于django的书
+        raise serializers.ValidationError("这不是一本关于djangod的书！")
+
+
+
+
+class BookInfoSerializer(serializers.Serializer):
+    """图书数据序列化器"""
+    # read_only设置为True表示该字段只作用于序列化，反序列化的时候直接忽略
+    id = serializers.IntegerField(label='ID', read_only=True)
+
+    # write_only设置为True表示该字段只作用于反序列化，序列化的时候直接忽略
+    btitle = serializers.CharField(label='名称',
+                                   max_length=20,
+                                   min_length=2,
+
+                                   # validators约束条件指定多个针对当前字段的校验函数
+                                   validators=[check_btitle]
+    )
+
+
+    bpub_date = serializers.DateField(label='发布日期', required=True)
+
+    bread = serializers.IntegerField(label='阅读量', required=False, min_value=0)
+    bcomment = serializers.IntegerField(label='评论量', required=False, min_value=0)
+    image = serializers.ImageField(label='图片', required=False, allow_null=True)
+    is_delete  =serializers.BooleanField(required=False)
+
+    # heros隐藏字段，多个从表HeroInfo对象
+    # heros = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    # heros = serializers.StringRelatedField(many=True)
+    heros = HeroInfoSerializer2(many=True)
+
+class HeroInfoSerializer(serializers.Serializer):
+    """英雄数据序列化器"""
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female')
+    )
+
+    # HeroInfo的固有字段/属性
+    id = serializers.IntegerField(label='ID', read_only=True)
+    hname = serializers.CharField(label='名字', max_length=20)
+    hgender = serializers.ChoiceField(choices=GENDER_CHOICES, label='性别', required=False)
+    hcomment = serializers.CharField(label='描述信息', max_length=200, required=False, allow_null=True)
+    is_delete = serializers.BooleanField()
+
+    # 外间关联属性
+    # hbook是当前英雄对象关联的"唯一"的主表"BookInfo对象"
+
+    # (1)如果想把关联字段，序列化成关联对象数据的主键; read_only=True当前字段只作用于序列化操作
+    # hbook = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    # (2) 把关联字段，序列化成它的__str__方法返回的结果
+    # {
+    #     "hname": xxx,
+    #     "hgener": xxx,
+    #     ...
+    #     "hbook": "射雕英雄传"
+    # }
+    hbook = serializers.StringRelatedField() # 无约束条件，默认read_only=True
+
+    # (3) 关联字段自定义序列化
+    # {
+    #     "hname": xxx,
+    #     "hgener": xxx,
+    #     ...
+    #     "hbook": {"btitle": xxx, "bpub_date": xxx}
+    # }
+    # hbook = BookInfoSerializer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
